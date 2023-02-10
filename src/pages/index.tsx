7ef120c6ba6d064b27c3c90835c06e08b1e187f1
Phil Leggetter <phil@leggetter.co.uk>
2023-02-10 16:40:57 +0000

    chore: style updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,48 +34,130 @@ export default function IndexPage() {
 
   if (!user.data) {
     return (
-      <div style={styles}>
+      <div style={styles.container}>
         <h1>Loading...</h1>
       </div>
     );
   }
   return (
-    <div style={styles}>
-      <h1>NVG Social</h1>
-      <h2>{`${user.data?.id}: ${user.data?.name}`}</h2>
+    <div style={styles.container}>
+      <main style={styles.main as React.CSSProperties}>
+        <header>
+          <h1>NVG Social</h1>
+          <h2>Welcome, {user.data?.name}</h2>
+        </header>
 
-      <form onSubmit={handleSubmit}>
-        <textarea
-          id="message"
-          onChange={(e) => setMessage(e.target.value)}
-          readOnly={submitting}
-          value={message}
-        ></textarea>
-        <input type="submit" value="Post" disabled={submitting} />
-      </form>
-
-      <section className="messages">
-        {posts.map((post) => {
-          return (
-            <div key={post.id}>
-              <div>
-                <span>{post.name}:</span>
-                <span>{post.text}</span>
-              </div>
-              <div>
-                {post.createdAt
-                  ? new Date(post.createdAt).toISOString()
-                  : "no date set"}
-              </div>
+        <section>
+          <form style={styles.form} onSubmit={handleSubmit}>
+            <textarea
+              id="message"
+              onChange={(e) => setMessage(e.target.value)}
+              readOnly={submitting}
+              value={message}
+              style={styles.textArea as React.CSSProperties}
+              required={true}
+              placeholder="What's on your mind?"
+            ></textarea>
+            <div style={styles.actions}>
+              <input
+                type="submit"
+                value="Post"
+                disabled={submitting}
+                style={styles.submitBtn}
+              />
             </div>
-          );
-        })}
-      </section>
+          </form>
+        </section>
+
+        <section className="messages">
+          {posts.map((post) => {
+            return (
+              <div key={post.id} style={styles.post}>
+                <div style={styles.username}>{post.name}</div>
+                <div style={styles.postText}>{post.text}</div>
+                <div>
+                  <span style={styles.timestamp}>
+                    {post.createdAt
+                      ? new Date(post.createdAt).toISOString()
+                      : "no date set"}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </main>
     </div>
   );
 }
 
+const fontFamily =
+  "ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color emoji";
 const styles = {
-  width: "100vw",
-  height: "100vh",
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: fontFamily,
+    fontSize: "16px",
+    width: "100%",
+  },
+
+  main: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    width: 400,
+  },
+
+  form: {},
+
+  textArea: {
+    fontFamily: fontFamily,
+    width: "100%",
+    height: 18 * 5,
+    fontSize: "16px",
+    lineHeight: "18px",
+    border: "none",
+    borderBottom: "1px solid #e5e7eb",
+    overflow: "auto",
+    outline: "none",
+    boxShadow: "none",
+    resize: "none",
+  },
+
+  actions: {
+    display: "flex",
+    justifyContent: "flex-end",
+    padding: "10px 0",
+    borderBottom: "1px solid #e5e7eb",
+  },
+
+  submitBtn: {
+    border: 0,
+    borderRadius: 10,
+    width: 75,
+    height: 30,
+    cursor: "pointer",
+  },
+
+  post: {
+    borderRadius: "20px",
+    backgroundColor: "#A6ECD9",
+    padding: 10,
+    margin: "10px 0",
+  },
+
+  postText: {
+    margin: "10px 0",
+  },
+
+  username: {
+    marginRight: 5,
+    fontWeight: 600,
+  },
+
+  timestamp: {
+    marginTop: 5,
+    fontSize: "0.5em",
+  },
 };
