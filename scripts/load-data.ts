ef019d06ed7ac471f47660e5344d4481fa50c670
Phil Leggetter <phil@leggetter.co.uk>
2023-02-14 00:46:54 +0000

    feat: load sample data script

--- scripts/load-data.ts
+++ scripts/load-data.ts
@@ -8,6 +8,7 @@ import Post from "../src/db/models/post";
 import path from "node:path";
 
 const BATCH_SIZE = 200;
+const USERNAMES = ["leggetter", "ovaistariq", "adilansari", "GarrenSmith"];
 
 async function main() {
   const mockFilesDir = path.join(__dirname, "..", "mock-data");
@@ -15,9 +16,13 @@ async function main() {
   const postCollection = tigrisClient.getDatabase().getCollection<Post>(Post);
   for (let i = 0; i < mockFiles.length; ++i) {
     console.log(`Reading mock file ${i + 1} of ${mockFiles.length}`);
-    const json = await fs.readFile(path.join(mockFilesDir, mockFiles[i]), "utf-8");
+    let json = await fs.readFile(path.join(mockFilesDir, mockFiles[i]), "utf-8");
 
-    const posts: Post[] = JSON.parse(json);
+    let posts: Post[] = JSON.parse(json);
+    posts = posts.map((post) => {
+      post.username = USERNAMES[Math.floor(Math.random() * USERNAMES.length)];
+      return post;
+    })
     while (posts.length > 0) {
       const insert = posts.splice(0, BATCH_SIZE);
       await postCollection.insertMany(insert);
