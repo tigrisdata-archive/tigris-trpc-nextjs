02e9b8f50da613032914585ebab7abfaa50c1ad8
Phil Leggetter <phil@leggetter.co.uk>
2023-02-21 15:11:09 +0000

    chore: es-lint fix for post-card & posts-list

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,22 +1,21 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-// import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
-import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { DateTime } from "luxon";
-import Post from "~/db/models/post";
+import type Post from "~/db/models/post";
 import { getAvatarCharacter } from "~/utils/text-utils";
 import { MuiNextLink } from "./mui-next-link";
 
-export default function PostCard({ post }: { post: Post }) {
+export default function PostCard({ post }: { post: Post }): JSX.Element {
   const avatarChar = getAvatarCharacter(post.username);
 
   return (
@@ -51,19 +50,13 @@ export default function PostCard({ post }: { post: Post }) {
           </MuiNextLink>
         }
         subheader={
-          post.createdAt
+          post.createdAt !== undefined
             ? DateTime.fromISO(post.createdAt.toString()).toLocaleString(
                 DateTime.DATETIME_MED
               )
             : ""
         }
       />
-      {/* <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      /> */}
       <CardContent>
         <Typography variant="body2" color="text.secondary">
           {post.text}
--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import PostCard from "~/components/post-card";
-import Post from "~/db/models/post";
+import type Post from "~/db/models/post";
 
-export default function PostsList({ posts }: { posts: Post[] }) {
+export default function PostsList({ posts }: { posts: Post[] }): JSX.Element {
   return (
     <Box id="posts">
       <Stack spacing={2}>
