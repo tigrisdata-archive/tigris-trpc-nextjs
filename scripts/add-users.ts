1fc4423e94ff59d60e62acf45f9447fae722c41f
Phil Leggetter <phil@leggetter.co.uk>
2023-02-21 16:20:52 +0000

    chore: eslint script fixes

--- scripts/add-users.ts
+++ scripts/add-users.ts
@@ -4,7 +4,7 @@ loadEnvConfig(process.cwd(), process.env.NODE_ENV !== "production")
 import { tigrisClient } from "../src/utils/tigris"
 import User from "../src/db/models/user";
 
-async function main(usernames: string[]) {
+async function main(usernames: string[]): Promise<void> {
   if (!usernames.every(x => x.length >= 4)) {
     throw new Error('Usernames must be 4 or more characters long');
   }
@@ -13,7 +13,15 @@ async function main(usernames: string[]) {
   const users: User[] = usernames.map(username => { return { username } });
   const usersCollection = tigrisClient.getDatabase().getCollection<User>(User);
   await usersCollection.insertMany(users);
-  console.log("Users inserted.");
+
 }
 
-main(process.argv.slice(2));
+main(process.argv.slice(2))
+  .then(async () => {
+    console.log("Users inserted.");
+    process.exit(0);
+  })
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  });
