63d6adf791046a962167ba8bd50b6460a6c7d914
Phil Leggetter <phil@leggetter.co.uk>
2023-02-21 17:34:47 +0000

    chore: more es-lint updates

--- src/components/mui-next-link.tsx
+++ src/components/mui-next-link.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import NextLink from "next/link";
-import Link, { LinkProps } from "@mui/material/Link";
+import Link, { type LinkProps } from "@mui/material/Link";
 
-export const MuiNextLink = (props: LinkProps) => {
+export const MuiNextLink = (props: LinkProps): JSX.Element => {
   return <Link component={NextLink} {...props} />;
 };
