d9475e531bd8ed3e67553abaa2f7fbe5120d0e04
Phil Leggetter <phil@leggetter.co.uk>
2023-02-21 16:34:21 +0000

    chore: removing tigris user from trpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,30 +1,12 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import { publicProcedure, router } from '~/server/trpc';
-import { tigrisClient } from '~/utils/tigris';
 import { z } from 'zod';
-import User from '~/db/models/user';
 import CONFIG from "~/config";
-
-// Since we're not implementing signup/login
-// default to using the first user returned
-// from the collection of Users
-let _defaultUser: User;
-const getDefaultUser = async (): Promise<User> => {
-  if (_defaultUser === undefined) {
-    const usersCollection = tigrisClient.getDatabase().getCollection<User>(User);
-    const user = await usersCollection.findOne();
-    if (user === undefined) {
-      throw new Error("A default user was expected to be founded.")
-    }
-    _defaultUser = user;
-  }
-  return _defaultUser;
-}
 
 const appRouter = router({
   getUser: publicProcedure
-    .query(async () => {
-      return await getDefaultUser();
+    .query(() => {
+      return { username: "staticUser" }
     }),
 });
 
