a59b2555fa3bfe8a2f69dbf0a79a310dd39750ee
Phil Leggetter <phil@leggetter.co.uk>
2023-02-23 11:24:54 +0000

    chore: part 1 complete

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,10 +3,30 @@ import { publicProcedure, router } from '~/server/trpc';
 import { z } from 'zod';
 import CONFIG from "~/config";
 
+import { type Cursor, FindQueryOptions, Order, Tigris } from "@tigrisdata/core";
+import User from '~/db/models/user';
+import Post from '~/db/models/post';
+
+const tigrisClient = new Tigris();
+const postsCollection = tigrisClient.getDatabase().getCollection<Post>(Post);
+
 const appRouter = router({
   getUser: publicProcedure
-    .query(() => {
-      return { username: "staticUser" }
+    .query(async () => {
+      const usersCollection = tigrisClient.getDatabase().getCollection<User>(User);
+      const user = await usersCollection.findOne();
+      return user;
+    }),
+
+  getMessages: publicProcedure
+    .query(async () => {
+      const cursor: Cursor<Post> = postsCollection.findMany({
+        sort: { field: "createdAt", order: Order.DESC },
+        options: new FindQueryOptions(CONFIG.DEFAULT_PAGING_SIZE, 0),
+      });
+
+      const results = await cursor.toArray();
+      return results;
     }),
 });
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typography } from "@mui/material";
 import { Layout } from "~/components/layout";
 import { Loading } from "~/components/loading";
 
 import { trpc } from "../utils/trpc";
+import type Post from "~/db/models/post";
+import PostsList from "~/components/posts-list";
 
 export default function IndexPage(): JSX.Element {
   const userPayload = trpc.getUser.useQuery();
   const user = userPayload.data as { username: string };
+  const queryPosts = trpc.getMessages.useQuery();
+
+  const [posts, setPosts] = useState<Post[]>([]);
 
   if (userPayload.data === undefined) {
     return <Loading />;
   }
+
+  useEffect(() => {
+    if (queryPosts.status === "success") {
+      setPosts(queryPosts.data as Post[]);
+    }
+  }, [queryPosts.data]);
 
   return (
     <Layout username={user.username}>
       <Typography variant="h4" component="h2" mb={5}>
         Welcome, {user.username}
       </Typography>
+
+      {queryPosts.status === "loading" && <Loading />}
+
+      {queryPosts.status === "success" && <PostsList posts={posts} />}
     </Layout>
   );
 }
